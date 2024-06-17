3154300d964c6299372b8bcbf2cbf0186ca733f3
unknown <DmitryAntipin151002>
2024-06-17 18:29:49 +0300

    version 1

---
+++ WebAplicationSpring/src/main/java/com/example/WebAplicationSpring/WebAplicationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebAplicationSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAplicationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAplicationSpringApplication.class, args);
+	}
+
+}
---
+++ WebAplicationSpring/src/test/java/com/example/WebAplicationSpring/WebAplicationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebAplicationSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAplicationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
